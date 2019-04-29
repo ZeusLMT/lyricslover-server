96fff7b1d121e05eb4f60477af33a921e4906a72
ZeusLMT <lemanhtuan275@gmail.com>
2019-04-29 11:32:07 +0300

    enable https redirecting

--- server.js
+++ server.js
@@ -12,16 +12,16 @@ app.use(helmet({ ieNoOpen: false }));
 
 //https redirecting
 
-// app.enable('trust proxy');
-// app.use ((req, res, next) => {
-//     if (req.secure) {
-//         // request was via https, so do no special handling
-//         next();
-//     } else {
-//         // request was via http, so redirect to https
-//         res.redirect('https://' + req.headers.host + req.url);
-//     }
-// });
+app.enable('trust proxy');
+app.use ((req, res, next) => {
+    if (req.secure) {
+        // request was via https, so do no special handling
+        next();
+    } else {
+        // request was via http, so redirect to https
+        res.redirect('https://' + req.headers.host + req.url);
+    }
+});
 
 //Use router
 app.use(require('./routers'));
